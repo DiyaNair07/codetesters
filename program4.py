c7283ad218ad02980bdc95fdc6e5651fe8184f56
DiyaNair07 <77486605+DiyaNair07@users.noreply.github.com>
2021-01-15 19:39:11 +0530

    Add files via upload

---
+++ program4.py
@@ -0,0 +1,10 @@
+x=int(input("Enter 1st number"))
+y=int(input("Enter 2nd number"))
+z=int(input("Enter 3rd number"))
+
+a=int((x+y)*z)
+if(a>100):
+  print("Greater than 100")
+
+else:
+  print("Lesser than 100")   
